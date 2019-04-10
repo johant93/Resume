daf07d69a4139e429621de7f4bb85e9085e769ab
johann thuillier <thuillierjohann@gmail.com>
2019-04-10 18:31:56 +0300

    server up

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.use('/css',express.static(__dirname +'/css'));
+app.use('/js',express.static(__dirname +'/js'));
+
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
 });
