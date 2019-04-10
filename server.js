4f7a09f3ef995dcba7b3a4686691b34216441a93
johann thuillier <thuillierjohann@gmail.com>
2019-04-10 17:07:20 +0300

    server up

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
-	res.render('index');
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.listen(port, function() {
