b80e27845c707288bbc8d26058208537f89a7f55
johann thuillier <thuillierjohann@gmail.com>
2019-04-10 17:10:04 +0300

    up

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
 
 app.listen(port, function() {
-	console.log('app running')
-})
+	console.log('app running');
+});
